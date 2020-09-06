866ea885769cd6c505cde5fcd6ac3f0b67906228
Nicolas Villetelle <nicolas.villetelle@groupe-cyllene.com>
2020-09-06 15:13:49 +0200

    Gestion partie reference

--- src/Form/MediaType.php
+++ src/Form/MediaType.php
@@ -4,16 +4,24 @@ namespace App\Form;
 
 use App\Entity\Media;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class MediaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('path')
-            ->add('reference')
+            ->add('file', FileType::class, [
+                'label' => 'Image :',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new Image()
+                ]
+            ])
         ;
     }
 
--- src/Form/ReferenceType.php
+++ src/Form/ReferenceType.php
@@ -47,6 +47,9 @@ class ReferenceType extends AbstractType
             ])
             ->add('medias', CollectionType::class, [
                 'entry_type' => MediaType::class,
+                'entry_options' => [
+                    'label' => false,
+                ],
                 'allow_add' => true,
                 'allow_delete' => true,
                 'by_reference' => false
